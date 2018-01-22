c3bf9b2374a2df0bfd302e241e36355745f40d81
Berend Weij <info@berendweij.nl>
2018-01-22 12:25:07 +0100

    adds cors

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require('express');
 // vervolgens maken we een instantie aan van express zodat we een webserver hebben
 const app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 // we regelen onze routes
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/students', (req, res) => res.json([{name:'Berend'}]));
+app.get('/', (req, res) => res.send('Dit is onze REST API'));
 
 // we zeggen dat onze app moet luisteren op poort 3000
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
